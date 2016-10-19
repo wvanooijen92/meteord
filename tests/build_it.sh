6d126cdeb6dc0263982c03b0267bbbee50d25000
Jesse Rosenberger <github@jro.cc>
2016-10-20 00:18:27 +0300

    Update tags for images to -node-4.4.7 suffix

--- tests/build_it.sh
+++ tests/build_it.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-docker build --no-cache -t meteorhacks/meteord:base ../base
-docker build --no-cache -t meteorhacks/meteord:onbuild ../onbuild
-docker build --no-cache -t meteorhacks/meteord:devbuild ../devbuild
-docker build --no-cache -t meteorhacks/meteord:binbuild ../binbuild
+
+set -x
+
+docker build --no-cache -t abernix/meteord:base-node-4.4.7 ../base
+docker build --no-cache -t abernix/meteord:onbuild-node-4.4.7 ../onbuild
+docker build --no-cache -t abernix/meteord:devbuild-node-4.4.7 ../devbuild
+docker build --no-cache -t abernix/meteord:binbuild-node-4.4.7 ../binbuild
