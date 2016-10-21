79d553b4f71c3535c88aa9ad6b5ce6a226624127
Jesse Rosenberger <github@jro.cc>
2016-10-21 11:44:18 +0300

    Allow running from root or here.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -5,14 +5,17 @@ set -x
 
 : ${NODE_VERSION?"NODE_VERSION has not been set."}
 
-./build_it.sh
+my_dir=`dirname $0`
+root_dir="$my_dir/.."
 
-./test_meteor_app.sh
+${root_dir}/build_it.sh
 
-./test_bundle_local_mount.sh
+${my_dir}/test_meteor_app.sh
+
+${my_dir}/test_bundle_local_mount.sh
 
 # This uses BUNDLE_URL from S3
-./test_bundle_web.sh
+${my_dir}/test_bundle_web.sh
 
-./test_phantomjs.sh
-./test_no_app.sh
+${my_dir}/test_phantomjs.sh
+${my_dir}/test_no_app.sh
