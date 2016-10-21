0a3a96b502858deac01d3ffd3b41a0071123da7e
Jesse Rosenberger <github@jro.cc>
2016-10-21 12:16:09 +0300

    Forgot to commit lib.

---
+++ tests/lib.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo "Testing shit"
+
+doalarm () { perl -e 'alarm shift; exec @ARGV' "$@"; }
+
+add_server_lookfor () {
+  cat <<EOM > server/main.js
+    require('meteor/meteor').Meteor.startup(() => console.log('$look_for'));
+EOM
+}
+
+watch_docker_logs_for () {
+  doalarm 5 sh -c \
+    'docker logs -f "$1" | grep --line-buffered -m1 "$2"'
+}
+
+check_server_for () {
+  curl -s "http://localhost:$1" | grep "${2}" 2>&1 > /dev/null
+}
