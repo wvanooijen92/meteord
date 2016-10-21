7c681e97a092c4ffc234f29efb16b3671ba0fc4d
Jesse Rosenberger <github@jro.cc>
2016-10-21 11:19:24 +0300

    Variablize app name

--- tests/test_meteor_app.sh
+++ tests/test_meteor_app.sh
@@ -3,14 +3,16 @@
 set -e
 set -x
 
+base_app_name=meteord-test-app
+
 trap "echo Failed: Meteor app" EXIT
 
 doalarm () { perl -e 'alarm shift; exec @ARGV' "$@"; }
 
 clean() {
-  docker rm -f meteor-app 2> /dev/null || true
-  docker rmi -f meteor-app-image 2> /dev/null || true
-  rm -rf meteord-app-test || true
+  docker rm -f "${base_app_name}" 2> /dev/null || true
+  docker rmi -f "${base_app_name}-image" 2> /dev/null || true
+  rm -rf ${base_app_name} || true
 }
 
 cd /tmp
@@ -18,8 +20,8 @@ clean
 
 look_for="==== METEORDTEST ===="
 
-meteor create meteord-app-test
-cd meteord-app-test
+meteor create "${base_app_name}"
+cd "${base_app_name}"
 echo "FROM abernix/meteord:base" > Dockerfile
 cat <<EOM > server/main.js
 require('meteor/meteor').Meteor.startup(() => console.log('$look_for'));
@@ -27,14 +29,17 @@ EOM
 
 test_root_url_hostname=yourapp_dot_com
 
-docker build -t meteor-app-image ./
+docker build -t "${base_app_name}" -image ./
 docker run -d \
-    --name meteor-app \
+    --name "${base_app_name}" \
     -e ROOT_URL=http://$test_root_url_hostname \
     -p 8080:80 \
-    meteor-app-image
+    "${base_app_name}-image"
 
-doalarm 5 sh -c 'docker logs -f meteor-app | grep --line-buffered -m1 "${look_for}"' || true
+doalarm 5 sh -c \
+  'docker logs -f ${base_app_name} | grep --line-buffered -m1 "${look_for}"' \
+  || true
+
 sleep 1
 curl -s http://localhost:8080 | grep ${test_root_url_hostname} 2>&1 > /dev/null
 clean
