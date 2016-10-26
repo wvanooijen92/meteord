b0e3f2e33bbc80fa24a7df7a408a1e4119a50ccb
Jesse Rosenberger <github@jro.cc>
2016-10-26 15:39:19 +0300

    Better runner script

--- tests/run_circleci_tests.sh
+++ tests/run_circleci_tests.sh
@@ -12,31 +12,30 @@ if [ -z "${CIRCLE_NODE_TOTAL}" ] || [ -z "${CIRCLE_NODE_INDEX}" ]; then
   echo "Not running on CircleCI"
   exit 1
 fi
+IFS="
+"
 
-our_normal_scripts="
+our_scripts="\
 ${my_dir}/test_meteor_app.sh
-${my_dir}/test_meteor_versions.sh
 ${my_dir}/test_bundle_local_mount.sh
 ${my_dir}/test_bundle_web.sh
 ${my_dir}/test_phantomjs.sh
 ${my_dir}/test_no_app.sh
 "
 
-our_version_scripts=$(
-    cat ${my_dir}/meteor_versions_to_test | \
-      xargs -n1 -I% echo ${my_dir}/test_meteor_app.sh %
-  )
+our_scripts="${our_scripts}$( \
+  cat ${my_dir}/meteor_versions_to_test | \
+  xargs -n1 echo ${my_dir}/test_meteor_app.sh
+)"
 
-if ! [ -z "$testfiles" ]; then
+our_work="$( \
+  echo "${our_scripts}" | \
+  awk "NR % ${CIRCLE_NODE_TOTAL} == ${CIRCLE_NODE_INDEX}"
+)"
+
+if [ -z "${our_work}" ]; then
   echo "more parallelism than tests"
   exit 0
 fi
 
-our_workload=$(echo "${our_normal_scripts} ${our_version_scripts}" |\
-  awk "NR % ${CIRCLE_NODE_TOTAL} == ${CIRCLE_NODE_INDEX}")
-
-IFS="
-"
-for test_script in "$our_workload"; do
-  sh -c "${test_script}"
-done
+echo "${our_work}" | tr '\n' '\0' | xargs -n1 -0 -I% -t sh -c "%"
