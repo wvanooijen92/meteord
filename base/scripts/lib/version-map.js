72310b02b73e112a4bc7ce2621d1ae1875f87a8c
Jesse Rosenberger <github@jro.cc>
2016-10-27 13:14:08 +0300

    Support multiple image recommendations.

--- base/scripts/lib/version-map.js
+++ base/scripts/lib/version-map.js
@@ -1,14 +1,20 @@
 module.exports = {
   "METEOR@1.4.1.1": {
     node: "4.4.7",
-    meteordImage: "abernix/meteord:node-4.4.7",
+    recommendedDockerImages: [
+      "abernix/meteord:base-node-4.4.7",
+    ],
   },
   "METEOR@1.4.1.2": {
     node: "4.6.0",
-    meteordImage: "abernix/meteord:node-4.6.0",
+    recommendedDockerImages: [
+      "abernix/meteord:base-node-4.6.0",
+    ],
   },
   "METEOR@1.4.1.3": {
     node: "4.6.1",
-    meteordImage: "abernix/meteord:node-4.6.1",
+    recommendedDockerImages: [
+      "abernix/meteord:base-node-4.6.1"
+    ],
   },
 };
