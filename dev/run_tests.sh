08c52226e6c5d48bdfbcc1d7f9a8cc162c5ce4c3
Jesse Rosenberger <github@jro.cc>
2016-10-27 20:54:23 +0300

    More tests

--- dev/run_tests.sh
+++ dev/run_tests.sh
@@ -14,17 +14,17 @@ my_dir=`dirname $0`
   export DOCKER_IMAGE_NAME_BASE
   export DOCKER_IMAGE_NAME_ONBUILD
 
-  # for meteor_version in `cat ${my_dir}/meteor_versions_to_test`; do
-  #   ${my_dir}/tests/test_meteor_app.sh "${meteor_version}"
-  # done
+  for meteor_version in `cat ${my_dir}/meteor_versions_to_test`; do
+    ${my_dir}/tests/test_meteor_app.sh "${meteor_version}"
+  done
 
   ${my_dir}/tests/test_bundle_local_mount.sh
 
   # # This uses BUNDLE_URL from S3.  Broken
   # ${my_dir}/tests/test_bundle_web.sh
 
-  # ${my_dir}/tests/test_phantomjs.sh
-  # ${my_dir}/tests/test_no_app.sh
+  ${my_dir}/tests/test_phantomjs.sh
+  ${my_dir}/tests/test_no_app.sh
 
   # Consider doing some trap cleanup here.
 )
