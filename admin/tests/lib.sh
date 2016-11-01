915eb39c108bee1260e827bb3006627c7086446a
Jesse Rosenberger <github@jro.cc>
2016-11-01 18:47:55 +0200

    Maybe just a matter of the error message?

--- admin/tests/lib.sh
+++ admin/tests/lib.sh
@@ -37,11 +37,13 @@ docker_logs_has () {
 }
 
 docker_logs_has_bcrypt_token () {
-  docker logs "$1" | grep -E '^bcrypt:::\$2[ay]?\$[0-9]{1,2}\$[^\$]{53}:::$'
+  docker logs "$1" | \
+    grep -E '^bcrypt:::\$2[ay]?\$[0-9]{1,2}\$[^\$]{53}:::$' 2>&1
 }
 
 watch_docker_logs_for () {
-  docker logs -f "$1" | grep --line-buffered -m1 "$2"
+  docker logs -f "$1" | \
+    grep --line-buffered -m1 "$2" 2>&1
 }
 
 watch_docker_logs_for_app_ready () {
