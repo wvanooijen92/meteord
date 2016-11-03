d33e5d0d0fb870970a47d8d7843a30e2f9a714c8
Jesse Rosenberger <github@jro.cc>
2016-11-03 21:40:25 +0200

    Revert "Revert "Put tests on port 80, which should break the build""
    This reverts commit 1b6f18c175ef1031ae2685138dfbf5602291e1c9.

--- admin/tests/test_bundle_web.sh
+++ admin/tests/test_bundle_web.sh
@@ -26,7 +26,7 @@ docker run -d \
     --name "${base_app_name}" \
     -e ROOT_URL=http://$test_root_url_hostname \
     -e BUNDLE_URL \
-    -p 63836:3000 \
+    -p 63836:80 \
     "${DOCKER_IMAGE_NAME_BUILDDEPS}"
 
 sleep 1
