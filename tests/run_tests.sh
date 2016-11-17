70893315c3ba30b1e24b9c5999cc562535f858a7
Jesse Rosenberger <github@jro.cc>
2016-11-17 15:12:04 +0200

    Remove localmount test which relies on something locally that circle doesn't have.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -7,8 +7,6 @@ bash ./build_it.sh
 bash ./test_meteor_app.sh
 bash ./test_meteor_app_with_devbuild.sh
 
-bash ./test_bundle_local_mount.sh
-
 # These use BUNDLE_URL from S3
 bash ./test_bundle_web.sh
 bash ./test_binary_build_on_base.sh
