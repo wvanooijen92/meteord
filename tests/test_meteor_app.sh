085e10152e58bc1ba03017968d9f17f07873306e
Wouter van Ooijen <info@vanooijen-it.nl>
2021-04-22 22:36:06 +0200

    updated image test

--- tests/test_meteor_app.sh
+++ tests/test_meteor_app.sh
@@ -6,6 +6,7 @@ set -x
 
 function clean() {
   docker rm -f meteor-app
+  docker rm -f mongo
   docker rmi -f meteor-app-image
   rm -rf hello
 }
@@ -19,17 +20,25 @@ echo "FROM wvanooijen92/meteord:node-${NODE_VERSION}-onbuild" > Dockerfile
 
 docker build -t meteor-app-image ./
 docker run -d \
+    --name mongo \
+    -p 27017:27017 \
+    --network=host \
+    mongo:latest
+
+docker run -d \
     --name meteor-app \
+    -e MONGO_URL=mongodb://localhost:27017/ \
     -e ROOT_URL=http://yourapp_dot_com \
+    --network=host \
     -p 8080:80 \
     meteor-app-image
 
 sleep 50
 
-appContent=`curl http://localhost:8080`
+appContent=`docker logs meteor-app`
 clean
 
-if [[ $appContent != *"yourapp_dot_com"* ]]; then
+if [[ $appContent != *"=> Starting meteor app on port:80"* ]]; then
   echo "Failed: Meteor app"
   exit 1
 fi
